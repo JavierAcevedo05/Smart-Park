a89cdcfd98ea6fa29c416c27711b88dcfd2386d1
Raul Sanabria Rodado <rsanrod1@epsg.upv.es>
2025-11-20 13:19:08 +0100

    Vinculado el proyecto con el google cloud

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,6 +46,6 @@ dependencies {
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
     implementation(platform("com.google.firebase:firebase-bom:34.4.0"))
-    implementation("com.google.android.gms:play-services-maps:18.1.0")
+    implementation("com.google.android.gms:play-services-maps:19.0.0")
     implementation("com.google.android.gms:play-services-location:21.0.1")
 }
