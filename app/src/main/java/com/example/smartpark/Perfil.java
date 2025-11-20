c58c3d5bf0c5e42bff4768675a98f38e8e27040a
Andreutale <amandreu3@gmail.com>
2025-11-20 20:57:13 +0100

    añdido "Acerca de" y mqtt (no va bien no se sabe por qué)

--- app/src/main/java/com/example/smartpark/Perfil.java
+++ app/src/main/java/com/example/smartpark/Perfil.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -115,5 +116,10 @@ public class Perfil extends AppCompatActivity {
             startActivity(intent);
             finish(); // Cierra la actividad actual
         });
+    }
+
+    public void lanzarAcercaDe(View view){
+        Intent i = new Intent(this, AcercaDeActivity.class);
+        startActivity(i);
     }
 }
